89f92ab471609a8355bea29f128d749396ccab32
Greg Tracy <gtracy@gmail.com>
2023-12-02 10:29:44 -0600

    improved reporting details for the validation task

--- bucket_validator/validate.js
+++ bucket_validator/validate.js
@@ -1,28 +1,26 @@
 const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3");
 const s3Client = new S3Client({
     region: "us-east-2",
-    //credentials: fromIni({ profile: 'memories' }),
  });
 
 const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 const sesClient = new SESClient({ 
     region: "us-east-2",
-    //credentials: fromIni({ profile: 'memories' }),
 });
 
 async function sendEmail(fromAddress, toAddresses, subject, body) {
   const params = {
-    Source: fromAddress, // The email address or domain you verified with Amazon SES
-    Destination: { // The destination for this email, composed of To:, CC:, and BCC: fields
-      ToAddresses: toAddresses, // An array of email addresses
+    Source: fromAddress, 
+    Destination: { 
+      ToAddresses: toAddresses, 
     },
-    Message: { // The message to send
-      Subject: { // The subject line of the email
+    Message: { 
+      Subject: {
         Data: subject,
         Charset: 'UTF-8',
       },
-      Body: { // The body of the email
-        Text: { // The content of the message, in text format
+      Body: {
+        Text: {
           Data: body,
           Charset: 'UTF-8',
         },
@@ -55,7 +53,7 @@ async function findMissingObjects(bucketName, folderName) {
                 ContinuationToken: continuationToken,
             };
             const response = await s3Client.send(new ListObjectsV2Command(listParams));
-            response.Contents.forEach((item) => {
+              response.Contents.forEach((item) => {
                 // Strip the prefix from the key to compare names
                 const itemName = item.Key.replace(Prefix, '');
                 if (Prefix) {
@@ -77,7 +75,10 @@ async function findMissingObjects(bucketName, folderName) {
     // Find all objects in the folder that do not have a matching name in the root
     const missingFiles = [...folderObjects].filter((name) => !rootObjects.has(name));
 
-    return missingFiles;
+    return {
+      missing: missingFiles,
+      root: [...rootObjects]
+    }
 }
 
 exports.handler = async (event) => {
@@ -91,9 +92,11 @@ exports.handler = async (event) => {
     console.dir(event);
 
     try {
-        const missingObjects = await findMissingObjects(bucketName, folderName)
-        console.log('Missing objects:', missingObjects);
-        const body = 'Missing objects in the parent directory:\n\n' + missingObjects.join('\n') + '\n\n';
+        const s3Objects = await findMissingObjects(bucketName, folderName)
+        console.log('Missing objects:', s3Objects.missing.length);
+        console.log('Root objects:',s3Objects.root.length);
+        const body = 'Root object count: ' + s3Objects.root.length + '\n\n' +
+                     'Missing objects in the parent directory:\n\n' + s3Objects.missing.join('\n') + '\n\n';
         
         await sendEmail(fromAddress, toAddresses, subject, body)
         console.log('Email sent');
